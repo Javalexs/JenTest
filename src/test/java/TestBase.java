9a175ea7f7374415b24066367aabed996a682056
Alexsey Fadeev <fadeevlesha@list.ru>
2023-01-09 13:56:42 +0300

    add driver+base

---
+++ src/test/java/TestBase.java
@@ -0,0 +1,26 @@
+import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+
+import static com.codeborne.selenide.Selenide.open;
+
+public class TestBase {
+    @BeforeAll
+    static void beforeAll(){
+        Configuration.baseUrl = "https://demoqa.com";
+        Configuration.browserSize = "1920x1080";
+        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
+    }
+    @BeforeEach
+    void addListener(){
+
+        SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
+    }
+    @AfterEach
+    void addAttachments(){
+
+    }
+}
