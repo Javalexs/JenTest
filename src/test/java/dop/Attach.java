76516c175b90288d39fb4e44d5a9c55d973f4962
Alexsey Fadeev <fadeevlesha@list.ru>
2023-01-23 09:21:38 +0300

    add attach

--- src/test/java/dop/Attach.java
+++ src/test/java/dop/Attach.java
@@ -42,7 +42,7 @@ public class Attach {
     }
 
     public static URL getVideoUrl() {
-        String videoUrl = "https://selenoid.autotests.cloud/video/" + sessionId() + ".mp4";
+        String videoUrl = System.getProperty("remoteUrl", "https://user1:1234@selenoid.autotests.cloud") + sessionId() + ".mp4";
 
         try{
             return new URL(videoUrl);
