ccea3d51e00c12c984778f1ecd8d0f180a7f8358
Alexsey Fadeev <fadeevlesha@list.ru>
2023-01-28 00:03:55 +0300

    add attach

--- src/test/java/prog/TestBase.java
+++ src/test/java/prog/TestBase.java
@@ -14,11 +14,11 @@ import static com.codeborne.selenide.Selenide.open;
 public class TestBase {
     @BeforeAll
     static void beforeAll(){
-        Configuration.browser = System.getProperty("browser", "сhrome");
-        Configuration.browserVersion = System.getProperty("browserVersion", "100");
-        Configuration.baseUrl = System.getProperty("baseUrl", "https://demoqa.com");
-        Configuration.browserSize = System.getProperty("browserSize", "1920x1080");
-        Configuration.remote = System.getProperty("remoteUrl", "https://user1:1234@selenoid.autotests.cloud/wd/hub");
+//        Configuration.browser = System.getProperty("browser", "сhrome");
+//        Configuration.browserVersion = System.getProperty("browserVersion", "100");
+        Configuration.baseUrl = "https://demoqa.com";
+        Configuration.browserSize = "1920x1080";
+        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
 
         DesiredCapabilities capabilities = new DesiredCapabilities();
         capabilities.setCapability("enableVNC", true);
